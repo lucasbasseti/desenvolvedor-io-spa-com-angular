48e79511a52f3ba6840c8f7bb9e22704400ba8ab
Lucas Basseti <lucas_basseti@hotmail.com>
2020-11-14 18:59:59 -0300

    Primeiro Commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { DataBindingComponent } from './demos/data-binding/data-binding.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
+
+// Definindo as rotas da aplicação.
+export const rootRouterConfig: Routes = [
+    // Redirecionamento raiz da aplicação.
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'contato', component: ContatoComponent },
+    { path: 'sobre', component: SobreComponent },
+    { path: 'feature-data-binding', component: DataBindingComponent },
+    { path: 'produtos', component: ListaProdutoComponent },
+    { path: 'produto-detalhe/:id', component: ListaProdutoComponent },
+];
---
+++ src/app/produtos/lista-produto/lista-produto.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Produto } from '../produto';
+import { ProdutoService } from '../produtos.service';
+
+@Component({
+  selector: 'app-lista-produto',
+  templateUrl: './lista-produto.component.html'
+})
+export class ListaProdutoComponent implements OnInit {
+
+  constructor(private produtoService: ProdutoService) { }
+
+  public produtos: Produto[];
+
+  ngOnInit(): void {
+
+    this.produtoService.obterProdutos()
+    .subscribe(
+      // success
+      produtos => {
+        this.produtos = produtos;
+        console.log(produtos);
+      },
+      // error
+      error => console.log(error)
+    );
+  }
+
+}
